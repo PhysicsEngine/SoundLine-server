af0f520226df673d27c010398416a9c8e51bd775
lewuathe <lewuathe@me.com>
2015-08-22 15:12:13 +0900

    Upload API

---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+var express = require('express');
+var multer = require('multer');
+var app = express();
+
+var upload = multer({dest: './uploads/'});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.get('/upload-debug', function(req, res) {
+    res.render('pages/upload-debug');
+});
+
+app.post('/upload', upload.single('file'), function(req, res, next) {
+    var tmpPath = req.file.path;
+    var targetPath = './uploads/' + req.file.originalname;
+    console.log(tmpPath);
+    fs.rename(tmpPath, targetPath, function(err) {
+        if (err) {
+            throw err;
+        }
+        res.send("File uploaded to " + targetPath);
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
