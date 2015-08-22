004501fe264e4dadbf71f5023fb130bcf5b2e9cd
lewuathe <lewuathe@me.com>
2015-08-22 15:57:51 +0900

    Download API

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.post('/upload', upload.single('file'), function(req, res, next) {
     });
 });
 
+app.get('/converted/:filename', function(req, res, next) {
+    var filename = req.params.filename;
+    console.log("Download " + filename);
+    res.download('./uploads/' + filename);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
